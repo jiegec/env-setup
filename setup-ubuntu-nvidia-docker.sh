8d8d0310329d0e53d4ce13579b4dbb72393f7f5d
Jiajie Chen <c@jia.je>
2021-12-27 20:57:48 +0800

    Add nvidia docker

---
+++ setup-ubuntu-nvidia-docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html#setting-up-nvidia-container-toolkit
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | xargs -d "\n" -L 1 add-apt-repository
