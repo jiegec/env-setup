44bfa903b0d7727f55283929885e38525c4f1fee
Jiajie Chen <c@jia.je>
2021-12-27 20:42:28 +0800

    Let user run sudo

--- setup-ubuntu-cuda.sh
+++ setup-ubuntu-cuda.sh
@@ -3,7 +3,7 @@
 VERSION=$(lsb_release -r | cut -f2)
 RELEASE="ubuntu${VERSION//.}"
 wget https://developer.download.nvidia.com/compute/cuda/repos/${RELEASE}/x86_64/cuda-${RELEASE}.pin
-sudo mv cuda-${RELEASE}.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/${RELEASE}/x86_64/7fa2af80.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/${RELEASE}/x86_64/ /"
-sudo apt-get update
+mv cuda-${RELEASE}.pin /etc/apt/preferences.d/cuda-repository-pin-600
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/${RELEASE}/x86_64/7fa2af80.pub
+add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/${RELEASE}/x86_64/ /"
+apt-get update
